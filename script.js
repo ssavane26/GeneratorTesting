4335617973c5ce87ee65ab8561471bba7a1f267d
ssavane26 <savaneprod@gmail.com>
2022-07-31 15:52:55 -0400

    Mods

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 var fetchButton = document.getElementById('fetch-button');
-
+var bio = document.getElementById('bio')
 function getApi() {
 
-  var requestUrl = '/random.json?language=en';
+   var requestUrl = 'https://uselessfacts.jsph.pl/random.json?language=en';
 
   fetch(requestUrl)
     .then(function (response) {
+    console.log(response.status)
       return response.json();
-    })}
+    })
+    .then(function (data){
+      console.log(data)
+      var finalText = document.getElementById('finalText')
+      finalText.textContent = data.text 
+      bio.appendChild(finalText)
+    })
+  }
     
 fetchButton.addEventListener('click', getApi);
+ 
